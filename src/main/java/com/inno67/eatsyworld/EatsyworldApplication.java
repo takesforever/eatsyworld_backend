912151e536797e2e82e540b87a24deb8dbf50ed6
takesforever <jhj.9342@gmail.com>
2022-08-12 17:18:00 +0900

    feat: 프로젝트 셋업

---
+++ src/main/java/com/inno67/eatsyworld/EatsyworldApplication.java
@@ -0,0 +1,13 @@
+package com.inno67.eatsyworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatsyworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EatsyworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/inno67/eatsyworld/EatsyworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inno67.eatsyworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatsyworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
