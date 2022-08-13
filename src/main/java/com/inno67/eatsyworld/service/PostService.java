0decd24b1a9ac0deb912fb73f6d75963a6f47de8
takesforever <jhj.9342@gmail.com>
2022-08-13 14:24:00 +0900

    #1 feat: post entity 생성 및 post 작성 기본 코드

--- src/main/java/com/inno67/eatsyworld/service/PostService.java
+++ src/main/java/com/inno67/eatsyworld/service/PostService.java
@@ -1,4 +1,24 @@
 package com.inno67.eatsyworld.service;
 
+import com.inno67.eatsyworld.dto.PostRequestDto;
+import com.inno67.eatsyworld.dto.PostResponseDto;
+import com.inno67.eatsyworld.model.Post;
+import com.inno67.eatsyworld.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import sun.net.www.MimeTable;
+
+import javax.transaction.Transactional;
+
+@Service
+@RequiredArgsConstructor
 public class PostService {
+    private final PostRepository postRepository;
+
+    //로그인 후 작성 가능하게 추후에 내용 추가
+    @Transactional
+    public Post writePost(PostRequestDto requestDto) {
+        Post post = new Post(requestDto);
+        return postRepository.save(post);
+    }
 }
